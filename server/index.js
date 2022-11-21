5e85b15a6c73455341303fd465386e3ed6780580
livinginatent <suleyman.eminbeyli@gmail.com>
2022-11-21 16:56:27 +0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
+
+
+
+const app = express();
+
+
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL =
+  "mongodb+srv://suleyman:Suzukigladius1_@cluster0.6v2idak.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.port || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>app.listen(PORT, () => console.log(`Server is running on: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+
